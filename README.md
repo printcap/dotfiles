85ec1a58dda5e969bebefbb35cbdf644eb54b8c3
Rene Mueller <muellren@gmail.com>
2019-01-02 09:54:24 +0100

    initial check-in

---
+++ README.md
@@ -0,0 +1,21 @@
+# Developement Environment
+
+Setup for development environment
+
+- zsh with Oh My Zsh
+- tmux
+- neovim
+
+## Ubuntu 18.04
+
+```bash
+sudo apt install get
+git clone https://github.com/printcap/dotfiles.git
+bash setup_ubuntu.sh
+```
+
+Run and quit with `:q` when done.
+
+```bash
+vim +PlugInstall +qall
+```
---
+++ setup_ubuntu.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o xtrace 
+set -o pipefail
+
+# Upgrade already installed packages
+sudo apt upgrade
+sudo apt update
+sudo apt install software-properties-common
+
+# Setup Environment
+# zsh and tmux
+sudo apt install zsh zsh-common tmux wget
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
+cp files ~/.zshrc ${HOME}/
+cp files ~/.tmux.conf ${HOME}/
+
+# neovim
+sudo apt install vim vim-common vim-runtime exuberant-ctags ncurses-term curl
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt install python-dev python-pip python3-dev python3-pip neovim
+NEOVIM_CONFIG_DIR = ${HOME}/.config/nvim
+mkdir -p ${NEOVIM_CONFIG_DIR}
+cp files/.config/nvim/init.vim ${NEOVIM_CONFIG_DIR}
+echo "\n\n\nExecute"
+echo "vim +PlugInstall +qall"
+
+
